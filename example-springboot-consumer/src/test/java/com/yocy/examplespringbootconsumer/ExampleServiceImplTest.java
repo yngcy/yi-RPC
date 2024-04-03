8b99d4efd91ce41ee28ffc230f3c0c0fbaba7fb4
yngcy <2792783811@qq.com>
2024-04-03 18:25:27 +0800

    feat:启动机制和注解驱动

--- example-springboot-consumer/src/test/java/com/yocy/examplespringbootconsumer/ExampleServiceImplTest.java
+++ example-springboot-consumer/src/test/java/com/yocy/examplespringbootconsumer/ExampleServiceImplTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
+import javax.annotation.Resource;
+
 /**
  * 单元测试
  */
